cfc5286c50fdb62fe25ee0390c9267216d98a462
Angela Cui <99048369+ayc325@users.noreply.github.com>
2024-01-04 13:03:01 -0800

    Challenge 4

--- index.js
+++ index.js
@@ -65,19 +65,21 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()} Contributions`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-
+const totalAmountRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `$${totalAmountRaised.toLocaleString()} Raised`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+const totalGames = GAMES_JSON.length;
+gamesCard.innerHTML = `${totalGames} Games`;
 
 
 /*************************************************************************************
